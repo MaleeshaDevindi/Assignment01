9e844541a6a4a5a76fbdb9d8ad4a9a1cb653c2b7
Maleesha Devindi <devindi-ct19016@stu.kln.ac.lk>
2023-12-05 17:18:41 +0530

    Add Feature - 05 - TextAlignment

---
+++ script.js
@@ -0,0 +1,15 @@
+function left(){
+    document.execCommand('justifyleft', false, null);
+}
+
+function center(){
+    document.execCommand('justifycenter', false, null);
+}
+
+function right(){
+    document.execCommand('justifyright', false, null);
+}
+
+function justify(){
+    document.execCommand('justifyfull', false, null);
+}
